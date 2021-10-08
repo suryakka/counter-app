0fd85d1924d163b3f2a48aef6f7e3c099e949153
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-08 12:41:57 +0700

    [CHORES] provide BlocProvider in MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,23 @@
+import 'package:counter_app/common/config/injector.dart';
 import 'package:counter_app/common/constants/common_constant.dart';
+import 'package:counter_app/presentation/bloc/counter_bloc.dart';
 import 'package:counter_app/presentation/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: CommonConstant.appTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: CommonConstant.appSubtitle),
-    );
+        title: CommonConstant.appTitle,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: BlocProvider(
+          create: (context) => getIt<CounterBloc>(),
+          child: MyHomePage(title: CommonConstant.appSubtitle),
+        ));
   }
 }
