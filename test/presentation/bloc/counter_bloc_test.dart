efd88b36d12966126efbfc9c8d81de7253373700
SuryaKKA <surya.arif@aux.dkatalis.com>
2021-10-08 11:12:45 +0700

    [TDD-RED] increment map event to state test

---
+++ test/presentation/bloc/counter_bloc_test.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:counter_app/presentation/bloc/counter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  CounterBloc counterBloc;
+  setUp(() {
+    counterBloc = CounterBloc();
+  });
+
+  group('Counter Bloc Test', () {
+    test('initial state', () {
+      expect(counterBloc.initialState, CounterInitial());
+    });
+
+    blocTest('Increment map event to state test',
+        build: () async => counterBloc,
+        act: (bloc) => bloc.add(Increment(1)),
+        expect: [CounterLoaded(1)]);
+  });
+}
